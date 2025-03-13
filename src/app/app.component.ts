5d985cf0bb46b569ad563c437a15718e7c8eb2bc
emmsdan <emmsdan.inc@gmail.com>
2025-03-13 11:45:11 +0100

    Setup repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes =  [
+    { path: 'products', component: ProductListComponent },
+    { path: 'cart', component: CartComponent },
+    { path: '', redirectTo: '/products', pathMatch: 'full' }
+  ];
